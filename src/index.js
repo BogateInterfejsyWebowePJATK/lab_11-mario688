20c66dd584d9e78645d4848a48afbccba96c4e19
mario688 <gliwamario@gmail.com>
2021-01-12 00:56:52 +0100

    zad1

---
+++ src/index.js
@@ -0,0 +1,14 @@
+
+import './index.scss';
+import React from "react";
+import ReactDOM, { render } from "react-dom";
+import {List} from "./components/List";
+
+
+
+
+
+ ReactDOM.render(
+     <List />,
+     document.getElementById("root")
+ );
